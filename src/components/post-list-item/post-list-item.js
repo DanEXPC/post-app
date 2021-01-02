6d433615bb89f48c4008e4cecb6b2e2d91ad7b5a
Danylo <Sherman2017Meizu@gmail.com>
2021-01-02 10:35:13 +0200

    creating a structure for an app

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className="app-list-item-label">
+                Hello World!
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button 
+                type="button" 
+                className="brn-star btn-sm">
+                    <i className="fa fa-star"></i>
+                </button>
+                <button 
+                type="button" 
+                className="brn-trash btn-sm">
+                    <i className="fa fa-trash-o"></i>
+                </button>
+                <i className="fa fa-heart"></i>
+            </div>
+        </li>
+    )
+}
+
+export default PostListItem
