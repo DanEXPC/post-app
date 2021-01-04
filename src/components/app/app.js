2a42c582c78b8f58b62abbaf694687044bf8e20e
Danylo <Sherman2017Meizu@gmail.com>
2021-01-04 09:00:04 +0200

    adding post removal functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
 import PostStatusFilter from '../post-status-filter'
@@ -7,25 +7,43 @@ import PostAddForm from '../post-add-form'
 
 import './app.css'
 
-const App = () => {
+export default class App extends Component {
 
-    const data = [
-        {label: 'Going to learn React', important: false, id: 'a1a1'},
-        {label: 'That is so good', important: false, id: 'a1a2'},
-        {label: 'I need a break...', important: false, id: 'a1a3'},
-    ]
+    state = {
+        data: [
+            {label: 'Going to learn React', important: false, id: 'a1a1'},
+            {label: 'That is so good', important: false, id: 'a1a2'},
+            {label: 'I need a break...', important: false, id: 'a1a3'},
+        ]
+    }
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            const index = data.findIndex((elem) => elem.id === id)
+
+            const newArray = [...data.slice(0, index),
+                              ...data.slice(index + 1)]
+
+            return {
+                data: newArray
+            }
+        })
+    } 
+    
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                    posts={this.state.data}
+                    onDelete={this.deleteItem} />
+                <PostAddForm/>
             </div>
-            <PostList posts={data} />
-            <PostAddForm/>
-        </div>
-    )
+        )
+    }
+    
 }
-
-export default App
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,7 +3,7 @@ import PostListItem from '../post-list-item'
 
 import './post-list.css'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
     const elements = posts.map((item) => {
 
@@ -11,11 +11,13 @@ const PostList = ({posts}) => {
             const {id, ...itemProps} = item
             return (
                 <li key={id} className='list-group-item'>
-                    <PostListItem {...itemProps} />
+                    <PostListItem 
+                        {...itemProps}
+                        onDelete={() => onDelete(id)} />
                 </li>
             )
         }
-        return ''
+        return null
     })
 
     function isEmpty(obj) {
