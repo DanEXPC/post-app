83aad4aa4d6839e08e5974c30830df1db24313b4
Danylo <danylosmahliuk@gmail.com>
2021-01-07 20:36:16 +0200

    modification of the publication of posts

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import './post-add-form.css'
 
-const PostAddForm = ({onAdd}) => {
-    return (
-        <div className="bottom-panel d-flex">
-            <input
-                type="text"
-                placeholder="What's up?"
-                className="form-control new-post-label"
-            />
-            <button
-                type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => onAdd('Hello') }>
-                Post</button>
-        </div>
-    )
-} 
+export default class PostAddForm extends Component {
 
-export default PostAddForm
+    state = {
+        text: ''
+    }
+
+    onValueChange = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        this.props.onAdd(this.state.text)
+        this.setState({ text: ''})
+    }
+
+    render () {
+        return (
+            <form 
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    placeholder="What's up?"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-outline-secondary">
+                    Post</button>
+            </form>
+        )
+    }
+} 
